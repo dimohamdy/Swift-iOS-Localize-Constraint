4f5d6da273dd24542aa899760436445ad08695d8
Ahmed Hamdy <dimo.hamdy@gmail.com>
2015-10-09 00:25:49 +0200

    Initial Commit

---
+++ Swift_iOS-Localize-Constraint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_iOS-Localize-Constraint
+//
+//  Created by binaryboy on 10/9/15.
+//  Copyright (c) 2015 AhmedHamdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
